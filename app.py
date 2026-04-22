58bf75eacc94cc67df9bf0fc762598db696f7c49
Shravani <shravanigoud49@gmail.com>
2026-04-22 08:10:32 +0530

    deploy setup

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import Flask, render_template, request, redirect
 import mysql.connector
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 # ✅ FIRST create app
 app = Flask(__name__)
